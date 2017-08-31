812b37abca956cb91da83e52af5d953267e11c3c
Harish <hkantheti@yodlee.com>
2017-08-31 13:45:23 +0530

    version at end of tutorial

---
+++ webapp/forms.py
@@ -0,0 +1,13 @@
+# Django imports
+from django import forms
+
+
+class NameForm(forms.Form):
+    your_name = forms.EmailField(label='Your name', max_length=100)
+
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
